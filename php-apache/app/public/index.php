018b4e5d5c1b29c3b1d6ccd0f00536ff46d3915a
yuka <ozaki043005031219@gmail.com>
2023-02-20 12:18:56 +0900

    root&host修正

--- php-apache/app/public/index.php
+++ php-apache/app/public/index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dbh = new PDO('mysql:dbname=docker_review;host=localhost;unix_socket=/tmp/mysql.sock', 'root', '0430');
+    $dbh = new PDO('mysql:dbname=docker_review;host=localhost:8888;unix_socket=/tmp/mysql.sock', 'root', 'root');
     echo '<h1 style="color:yellow">Congratulations</h1>';
 } catch (PDOException $e) {
     echo '<h1 style="color:red">Failed<br><p>' . $e->getMessage() . '</p>';

c6694a14df975c3d2fca96375f00d84677529eb0
yuka <ozaki043005031219@gmail.com>
2023-02-20 13:12:32 +0900

    パスワード変更

--- php-apache/app/public/index.php
+++ php-apache/app/public/index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dbh = new PDO('mysql:dbname=docker_review;host=localhost:8888;unix_socket=/tmp/mysql.sock', 'root', 'root');
+    $dbh = new PDO('mysql:dbname=docker_review;host=localhost:8888;unix_socket=/tmp/mysql.sock', 'root', '0430');
     echo '<h1 style="color:yellow">Congratulations</h1>';
 } catch (PDOException $e) {
     echo '<h1 style="color:red">Failed<br><p>' . $e->getMessage() . '</p>';
